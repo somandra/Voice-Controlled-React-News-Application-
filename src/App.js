2bd39758e9aa250e4bf05d8ac60dc41392fa4ee6
warri00r <somandra.bhl2015@gmail.com>
2020-08-06 15:30:06 +0530

    Added instructions to news page

--- src/App.js
+++ src/App.js
@@ -42,6 +42,12 @@ const App = () => {
     return(
         <div>
             <div className={classes.logoContainer}>
+                 {newsArticles.length ? (
+                    <div className={classes.infoContainer}>
+                        <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+                        <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+                    </div>
+                ) : null}
                 <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
             </div>
             <NewsCards articles={newsArticles} activeArticle={activeArticle} />
